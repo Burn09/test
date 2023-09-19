653875e9e6c194be263da485c963fde58968160c
Burn09 <taneev91@gmail.com>
2023-09-19 15:13:55 +0200

    Update t.py

--- t.py
+++ t.py
@@ -1,13 +1,14 @@
-#comment
-#comment
-#comment
-#comment
+import 1
+#
+
+#
 l = [1,2,3]
 st1 = 'somestr'
 s = [3,2,1]
 def my_names():
-  a = 0
+  c = 0
   print 'name'
 print('SOME')
 d = {'fn':'fname', 'ln': 'lname'}
 g = 'asfsdaf'
+l = d['b']
