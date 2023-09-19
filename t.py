1282fd04bda93bcacbc261ed85b9cb06ab70d463
Burn09 <taneev91@gmail.com>
2023-09-19 15:12:15 +0200

    Modify

--- t.py
+++ t.py
@@ -6,8 +6,11 @@ l = [1,2,3]
 st1 = 'somestr'
 s = [3,2,1]
 def my_names():
-  c = 0
+  a = 0
+  b = 0
+  g = a+b
   print 'name'
+'s'
 print('SOME')
 d = {'fn':'fname', 'ln': 'lname'}
 g = 'asfsdaf'
